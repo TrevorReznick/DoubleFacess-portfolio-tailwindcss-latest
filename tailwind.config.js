28845bd3c4aa594388b3b174fe50b3ea79d4ccd3
Joseph K <double.facessss@gmail.com>
2023-11-13 18:36:49 +0100

    fixed color mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
@@ -9,7 +11,30 @@ module.exports = {
     "./nuxt.config.{js,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-light": "#F7F8FC",
+        "secondary-light": "#FFFFFF",
+        "ternary-light": "#f6f7f8",
+        "primary-dark": "#0D2438",
+        "secondary-dark": "#102D44",
+        "ternary-dark": "#1E3851",
+      },
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "5rem",
+          xl: "6rem",
+          "2xl": "8rem",
+        },
+      },
+    },
   },
-  plugins: [],
+  variants: {
+    extend: {
+      opacity: ["disabled"],
+    },
+  },
+  //plugins: [require("@tailwindcss/forms")],
 }
