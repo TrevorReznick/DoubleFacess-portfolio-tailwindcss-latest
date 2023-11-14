c068af844971147bc1c34a1d2a2463958c7682cf
Joseph K <double.facessss@gmail.com>
2023-11-14 08:38:05 +0100

    fixing

--- store/index.js
+++ store/index.js
@@ -4,6 +4,16 @@ import { v4 as uuidv4 } from "uuid";
 
 export const state = () => ({
   isMobile: false,
+  aboutMe: [
+    {
+      id: uuidv4(),
+      bio: "Sistemista per più di vent’anni e appassionato di tecnologie web sin dai primi anni 2000, con una profonda esperienza nello sviluppo di applicazioni web dinamiche in php e database relazionali, oltre che nell’approfondimento del linguaggio Javascript e dell’interazione con il backend attraverso Ajax. Dal 2018 ho abbracciato lo sviluppo di Node.js e delle librerie Vuejs e React, nonchè del framework Nuxt.js. Alla ricerca di nuovi stimoli e spunti, curioso e attento agli ultimi sviluppi che l'IA sta facendo in molti campi, sopratutto nellla nuova offerta di formidabili strumenti di sviluppo tutti in itinere.",
+    },
+    {
+      id: uuidv4(),
+      /*bio: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?",*/
+    },
+  ],
   categories: [
     {
       id: uuidv4(),
@@ -56,11 +66,11 @@ export const state = () => ({
 })
 
 export const getters = {
-    _getter: state => items => state[items]
+  _getter: state => items => state[items]
 }
 
 export const mutations = {
-    commitAppState(state, { key, value }) {
-        state[key] = value
-    }
+  commitAppState(state, { key, value }) {
+    state[key] = value
+  }
 }
--- store/projects.js
+++ store/projects.js
@@ -2,9 +2,17 @@
 import Vue from 'vue'
 
 export const state = () => ({
-    
+    my_projects: [
+        {
+            
+        }
+    ]
 })
 
-export const getters = {}
+export const getters = {
+    getProjectById: (state) => (id) => {
+        return state.projects.find((project) => project.id == id)
+    }
+}
 
 export const mutations = {}
