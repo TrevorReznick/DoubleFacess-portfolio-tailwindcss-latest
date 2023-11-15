c9bc9341e71f5a8f2317efe79cf7bfc8bc24a702
Tuo Nome <double.facessss@gmail.com>
2023-11-15 21:10:04 +0100

    added projectGrid projects, added store and assets/imgs

--- components/projectsGrid.vue
+++ components/projectsGrid.vue
@@ -43,7 +43,48 @@
             aria-label="Name"
           />
         </div>
-        <ProjectsFilter @change="selectedProject = $event" />
+        <projectsFilter @change="selectedProject = $event" />
+      </div>
+    </div>
+    <!-- Projects grid -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10">
+      <div
+        v-for="project in filteredProjects"
+        :key="project.id"
+        class="
+          rounded-xl
+          shadow-lg
+          hover:shadow-xl
+          cursor-pointer
+          mb-10
+          sm:mb-0
+          bg-secondary-light
+          dark:bg-ternary-dark
+        "
+        aria-label="Single Project"
+      >
+        <NuxtLink :to="`/projects/${project.id}`">
+          <div>
+            <img
+              :src="project.img"
+              :alt="project.title"
+              class="rounded-t-xl border-none"
+            />
+          </div>
+          <div class="text-center px-4 py-6">
+            <p
+              class="font-general-semibold text-xl text-ternary-dark dark:text-ternary-light font-semibold mb-2"
+            >
+              {{ project.title }}
+            </p>
+            <span
+              class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light"
+            >
+                {{ project.category }}
+            </span
+            >
+          </div>
+        </NuxtLink>
       </div>
     </div>
   </div>
@@ -64,14 +105,12 @@ export default {
     };
   },
   computed: {
-    /*
-    ...mapState({
-      projectsGridSettings
-    }),
-    */
     ...mapGetters({
       _getter: '_getter'      
     }),
+    projects() {
+      return this.$store.state.projects['my_projects']
+    },
     filteredProjects() {
       if (this.selectedProject) {
         return this.filterProjectsByCategory();
--- store/projects.js
+++ store/projects.js
@@ -4,8 +4,62 @@ import Vue from 'vue'
 export const state = () => ({
     my_projects: [
         {
-               
-        }
+            title: "Immobiliare di Falco",
+            id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccdb",
+            category: "Real Estate App",
+            img: require('static/img/projects/difalco-website.png'),
+            url: 'https://www.difalcoimmobiliare.it'
+        },
+        {
+            title: "CP Immobiliare di Falco",
+            id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccdc",
+            category: "CMS Agenzia Immobilare",
+            img: require('static/img/projects/difalco-auth.png'),
+            url: 'https://www.difalcoimmobiliare.it/admin'
+        },
+        {
+            title: "Shopping Play",
+            id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccdy",
+            category: "Web App",
+            img: require('static/img/projects/shop-home.png'),
+            url: 'https://shoppingplay.netlify.app/'
+        },
+        {
+            title: "Nodejs PWA Notification System",
+            id: "260d1271-f1f2-4bc9-a5eb-05a89c8ccc99",            
+            category: "Backend/Nodejs",
+            img: require('static/img/projects/domini-home.png'),
+            url: 'https://www.dominiacency.com'
+        },
+        {
+            title: "Gestionale Supremacy",
+            id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccdm",
+            category: "Mobile Application",
+            img: require('static/img/projects/suprem-dashb.png'),
+            url: 'https://www.dominiacency.com'
+        },
+        {
+            title: "My Todo",
+            id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccbb",      
+            category: "Web Application",
+            img: require('static/img/projects/todo1.png'),
+            url: 'https://todolist-96d98d.netlify.app/app'
+        },
+        {
+            title: "ES6 Javascript Porting",
+            id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccbb",
+            category: "Web Application",
+            img: require('static/img/projects/todo1.png'),
+            url: 'https://www.difalcoimmobiliare.it'
+        },
+        {
+            title: "Open jAjax Demo",
+            id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccbb",
+            category: "Web Application",
+            img: require('static/img/projects/todo1.png'),
+            url: 'https://www.difalcoimmobiliare.it',
+        }        
+        
     ]
 })
 
