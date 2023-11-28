e80b1c4567d0f7dca0e771149915084d6876510c
Tuo Nome <double.facessss@gmail.com>
2023-11-28 19:10:32 +0100

    editing project page

--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -1,7 +1,40 @@
 <template>
 <div class="container mx-auto">
     <!-- Check if there are projects and then load -->
-    <div v-if="project"></div>
+    <div v-if="project">
+        <!-- Project heading and meta info -->
+      <div>        
+        <a 
+          :href="externalLink(project)"
+          target="_blank"
+          class="font-general-medium text-left text-3xl sm:text-4xl font-bold text-primary-dark dark:text-primary-light mt-14 sm:mt-20 mb-7"
+        >
+          IS My Title
+        </a>
+        <div class="flex">
+          <div class="flex items-center mr-10 pt-3">
+            <i
+              data-feather="clock"
+              class="w-4 h-4 text-ternary-dark dark:text-ternary-light"
+            ></i>
+            <span
+              class="font-general-medium ml-2 leading-none text-primary-dark dark:text-primary-light"
+              >Nov 04, 22</span
+            >
+          </div>
+          <div class="flex items-center pt-3">
+            <i
+              data-feather="tag"
+              class="w-4 h-4 text-ternary-dark dark:text-ternary-light"
+            ></i>
+            <span
+              class="font-general-medium ml-2 leading-none text-primary-dark dark:text-primary-light"
+              > Frontend / Bootstrap / Nuxt / Api / Auth0 / Codeigniter</span
+            >
+          </div>
+        </div>
+      </div>
+    </div>
     <div v-else class="font-general-medium container mx-auto text-center">
       <h1>No projects yet</h1>
     </div>
@@ -26,7 +59,7 @@ export default {
         project() {
             return this.$store.getters['projects/getProjectById'](this.$route.params.id)
         },
-        url(obj) {
+        _url(obj) {
             return this.externalLink(obj)
         }
     },
