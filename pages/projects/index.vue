982ba15b76c74ad15a502594ea8748f15b004095
Tuo Nome <double.facessss@gmail.com>
2023-11-30 19:09:53 +0100

    added projects page

---
+++ pages/projects/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container mx-auto">
+    <projectsGrid />
+  </div>
+</template>
+
+<script>
+export default {
+  scrollToTop: true,
+  data: () => {
+    return {
+      // @todo
+    };
+  },
+  computed: {
+    // @todo
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
+
