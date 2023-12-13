80ee31ce560e9cd15606917db79f050e22eb38a9
TrevorReznick <135354265+TrevorReznick@users.noreply.github.com>
2023-12-13 13:32:58 +0100

    added appToolbox.vue

---
+++ components/appToolbox.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="block sm:flex sm:gap-10 mt-10 sm:mt-20">
+  <div class="text-center mx-auto sm:mx-0 sm:self-center"> <!-- Aggiunta classe `mx-auto` e `sm:self-center` -->
+    <p class="font-general-semibold text-2xl sm:text-5xl font-semibold mb-2 text-ternary-dark dark:text-ternary-light">toolbox</p>
+    <i class="devicon-apple-original text-ternary-dark dark:text-ternary-light text-lg custom-icon"></i>
+  </div>
+</div>
+
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  data: () => {
+    return {
+      // @todo
+    }
+  },
+  computed: {
+    ...mapState(['aboutMe']),
+  },
+}
+</script>
+
+<style scoped>
+.custom-icon {
+  font-size: 26px;
+}
+</style>
